e4c46749bc9d32b9c2f369383d5a1bda3439e6cf
jPruim <jasonpruim@gmail.com>
2025-05-06 22:15:15 -0400

    new card dragging

--- Scenes/Playspace/card_manager.gd
+++ Scenes/Playspace/card_manager.gd
@@ -1,27 +1,55 @@
-extends Node
+extends Node2D
 
+
+#properties
+#Card object being grabbed (NOT a bool)
+var card_being_dragged = null
+#store screen size
+var screen_size
+# Masks
+const CARD_COLLISION_MASK = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	
+	
 	pass # Replace with function body.
 
 
 
 func _input(event: InputEvent) -> void:
-	
 	#mouse events
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
-				print("left click")
+				var card = raycast_check_for_card()
+				if card:
+					card_being_dragged = card
 			else:
-				print("left click released")
+				card_being_dragged = null
 			
 
 # Card Collision detector
 func raycast_check_for_card():
 	var space_state = get_world_2d().direct_space_state
-	var parameters = PhysicsPointQueryParamteres2D.new()
+	var parameters = PhysicsPointQueryParameters2D.new()
+	parameters.position = get_global_mouse_position()
+	parameters.collide_with_areas = true
+	parameters.collision_mask = CARD_COLLISION_MASK
+	var result = space_state.intersect_point(parameters)
+	
+	#if over an area
+	if result.size() > 0:
+		return result[0].collider.get_parent()
+	return null	
+	
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	#set dragged card to mouse position
+	if card_being_dragged:
+		var mouse_pos = get_global_mouse_position()
+		card_being_dragged.position = Vector2(clamp(mouse_pos.x, 0, screen_size.x),clamp(mouse_pos.y, 0, screen_size.y))		
 	pass
--- Scenes/Playspace/playspace.gd
+++ Scenes/Playspace/playspace.gd
@@ -16,21 +16,8 @@ func _ready() -> void:
 	#var playerHand = PlayerHand.instantiate()
 	pass # Replace with function body.
 
-#func _input(event: InputEvent) -> void:
-	#
-	##mouse events
-	#if event is InputEventMouseButton:
-		#if event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
-			#var newCard = cardBase.instantiate()
-			#cardSelected = randi() % deckSize
-			#print(cardSelected)
-			#newCard.dbCardName = playerHand.cardList[cardSelected]
-			#newCard.position = get_global_mouse_position()
-			#newCard.scale = (cardSize)/newCard.size
-			#print(newCard.size)
-			#$Cards.add_child(newCard)
-			#playerHand.cardList.erase(playerHand.cardList[cardSelected])
-			#deckSize -= 1
+func _input(event: InputEvent) -> void:
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
