1ab36de2cd0c6d4f319e41baeafb2d0634a30fee
jPruim <jasonpruim@gmail.com>
2025-05-07 02:04:55 -0400

    card drag effect

--- Scenes/Cards/card_base.gd
+++ Scenes/Cards/card_base.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
-
+signal hovered
+signal hovered_off
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	get_parent().connect_card_signals(self)
 	pass
 
 
@@ -11,3 +13,13 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 
 	pass
+
+
+func _on_area_2d_mouse_entered() -> void:
+	emit_signal("hovered", self)
+	pass # Replace with function body.
+
+
+func _on_area_2d_mouse_exited() -> void:
+	emit_signal("hovered_off", self)
+	pass # Replace with function body.
--- Scenes/Playspace/card_manager.gd
+++ Scenes/Playspace/card_manager.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
 
-#properties
-#Card object being grabbed (NOT a bool)
+# Properties
+# Card object being grabbed (NOT a bool)
 var card_being_dragged = null
-#store screen size
-var screen_size
+# store screen size
+var screen_size: Vector2
+
+# Check if hovering
+var is_hovering_card :bool = false
+
 # Masks
 const CARD_COLLISION_MASK = 1
 
@@ -25,9 +29,9 @@ func _input(event: InputEvent) -> void:
 			if event.pressed:
 				var card = raycast_check_for_card()
 				if card:
-					card_being_dragged = card
+					start_drag(card)
 			else:
-				card_being_dragged = null
+				end_drag()
 			
 
 # Card Collision detector
@@ -39,9 +43,10 @@ func raycast_check_for_card():
 	parameters.collision_mask = CARD_COLLISION_MASK
 	var result = space_state.intersect_point(parameters)
 	
-	#if over an area
+	# if over an area
 	if result.size() > 0:
-		return result[0].collider.get_parent()
+		# make sure to highlight return top card only
+		return get_top_card(result)
 	return null	
 	
 
@@ -53,3 +58,62 @@ func _process(delta: float) -> void:
 		var mouse_pos = get_global_mouse_position()
 		card_being_dragged.position = Vector2(clamp(mouse_pos.x, 0, screen_size.x),clamp(mouse_pos.y, 0, screen_size.y))		
 	pass
+
+#Handle card Signals
+func connect_card_signals(card):
+	card.connect("hovered", on_hovered_card)
+	card.connect("hovered_off", on_hovered_off_card)
+
+# Handle card being hovered
+func on_hovered_card(card):
+	if !is_hovering_card:
+		is_hovering_card = true
+		card_affects(card, true)
+
+# Handle card being hovered
+func on_hovered_off_card(card):
+	if !card_being_dragged:
+		card_affects(card, false)
+		#check if transitioning onto a new card
+		var new_card = raycast_check_for_card()
+		if new_card:
+			card_affects(new_card, true)
+		else:
+			is_hovering_card = false
+	
+# Change card affects
+func card_affects(card, hovered: bool):
+	if hovered:
+		card.scale = Vector2(1.05, 1.05)
+		card.z_index = 2
+	else:
+		card.scale  = Vector2( 1, 1)
+		card.z_index = 1
+		
+# Returns the top card from a raycast result
+func get_top_card(results):
+	# Should NEVER pass, but error check
+	if !results:
+		print("called get_top_card() on Null")
+		return
+		
+	# Calculate the top card (by z-index), if in doubt, choose "default" order
+	var top_card = results[0].collider.get_parent()
+	var check_card
+	for i in range(1, results.size()):
+		check_card = results[i].collider.get_parent()
+		if check_card.z_index > top_card.z_index:
+			top_card = check_card
+	return top_card
+		
+func start_drag(card):
+	card_being_dragged = card
+	card_being_dragged.scale = Vector2( 1, 1)
+	
+	
+func end_drag():
+	card_being_dragged.scale = Vector2( 1.05, 1.05)
+	card_being_dragged = null
+	
+	
+	
