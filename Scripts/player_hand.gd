9b7b4fb4eca11e2177f8408dd685b70283f67352
jPruim <jasonpruim@gmail.com>
2025-07-30 17:31:05 -0400

    PlayerHand: Bugfix - z_index

--- Scripts/player_hand.gd
+++ Scripts/player_hand.gd
@@ -16,6 +16,7 @@ var hovered: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	center_screen_x = floor(get_viewport().size.x / 2)
+	$".".z_index = Globals.Z_INDEX.hand
 
 # Add card to player hand
 func add_card_to_hand(card, speed = DEFAULT_ASPEED):
