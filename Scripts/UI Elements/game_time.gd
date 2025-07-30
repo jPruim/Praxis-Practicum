97ae7fb537a77e70b1821c8db20b0c5d04ef1409
jPruim <44886135+jPruim@users.noreply.github.com>
2025-07-30 18:13:15 -0400

    GameTime: Script start

--- Scripts/UI Elements/game_time.gd
+++ Scripts/UI Elements/game_time.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var current_time
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +10,27 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func set_time( time: int ):
+	if time == 3:
+		$Hourglass3.visible = true
+		$Hourglass2.visible = false
+		$Hourglass1.visible = false
+		current_time = 3
+	elif time == 2:
+		$Hourglass3.visible = false
+		$Hourglass2.visible = true
+		$Hourglass1.visible = false
+		current_time = 2
+	elif time == 1:
+		$Hourglass3.visible = false
+		$Hourglass2.visible = false
+		$Hourglass1.visible = true
+		current_time = 1
+	else:
+		$Hourglass1.visible = false
+		$Hourglass1.visible = false
+		$Hourglass1.visible = false
+		
+		
+		
