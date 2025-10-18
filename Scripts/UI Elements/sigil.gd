ad15bcc79a6ca935022859db258ddb2e827614ea
jPruim <jasonpruim@gmail.com>
2025-10-17 22:53:50 -0400

    Sigil: pre-implementation work

---
+++ Scripts/UI Elements/sigil.gd
@@ -0,0 +1,14 @@
+class_name Sigil
+extends Node2D
+
+@export var blue_gem_location: Vector2 = Vector2(-15, -40)
+@export var red_gem_location: Vector2 = Vector2(15, -40)
+@export var coin_location: Vector2 = Vector2(-15, -20)
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
